dc26108190e05e29219caf48a47d4d9d99b3611b
psilotec <scottadevito@gmail.com>
2016-06-18 16:08:41 -0400

    Set Up Styling for PersonDetail
    Used props and HTML with JSX variables to create styling for
    PersonDetail component.  Created a const for people properties to make
    JSX more readable.

--- client/components/person_detail.js
+++ client/components/person_detail.js
@@ -3,10 +3,19 @@
  */
 import React from 'react';
 
-const PersonDetail = (props) => {
+const PersonDetail = ({ people }) => {
+    const { name, email, phone, avatar } = people;
+
     return (
-        <div>
-            Person Detail
+        <div className="thumbnail">
+            <img src={avatar} />]
+            <div className="caption">
+                <h3>{name}</h3>
+                <ul className="list-group">
+                    <li className="list-group-item">Email: {email}</li>
+                    <li className="list-group-item">Phone: {phone}</li>
+                </ul>
+            </div>
         </div>
     );
 };
